1ceb1fb7fd40d0427ee7ebbf3424f0ecd2e3b4d5
dickermoshe <75931499+dickermoshe@users.noreply.github.com>
2023-01-20 15:17:42 -0500

    Bump

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setup(
     name="livigent-ca-patch",
-    version='0.1.0',
+    version='0.2.0',
     author="Moshe Dicker",
     author_email='dickermoshe@gmail.com',
     description="Make requests, pip and dependant packages work on devices with Livigent.",
